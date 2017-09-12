95ab4032c3203a3a75e9ecc29952f731a6b79311
jiahongyang <hongyooo@icloud.com>
2017-09-12 10:58:04 +0800

    version 1.1.1

--- GraceOS/app/src/main/java/com/grace/example/MainActivity.java
+++ GraceOS/app/src/main/java/com/grace/example/MainActivity.java
@@ -1,8 +1,14 @@
 package com.grace.example;
 
+import android.app.*;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.grace.Grace;
 
@@ -12,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Grace grace = new Grace(this);
+
+
+
+         final Grace grace = new Grace(this);
         grace.cover();
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -21,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
                 grace.rest();
             }
         },5000);
+    }
+
+    private void syncNow(){
+        Toast.makeText(this,"yeah yeah yeah ",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.e("dispatchTouchEvent","dispatchTouchEvent");
+        return super.dispatchTouchEvent(ev);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.e("MainActivity","onKeyDown");
+        return super.onKeyDown(keyCode, event);
     }
 }
--- GraceOS/grace/src/main/java/com/grace/OsTelevision.java
+++ GraceOS/grace/src/main/java/com/grace/OsTelevision.java
@@ -1,10 +1,17 @@
 package com.grace;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by hongyang on 17-8-25.
@@ -16,8 +23,10 @@ public class OsTelevision extends FrameLayout {
     private LinearLayout osLodeError;
     public static final int LODE_FLAG = 0x00;
     public static final int LODE_ERROR_FLAG = 0x01;
+    private static final String METHOD = "onAgain";
+    private int conversion;
 
-    public OsTelevision(Context context) {
+    public OsTelevision(Activity context) {
         super(context);
         this.initialization();
     }
@@ -28,13 +37,15 @@ public class OsTelevision extends FrameLayout {
     }
 
     private void initialization() {
+        setFocusable(true);
         LayoutInflater.from(getContext()).inflate(R.layout.os_television, this);
         osLode = (LinearLayout) findViewById(R.id.os_lode);
         osLodeError = (LinearLayout) findViewById(R.id.os_lode_error);
     }
 
     public void conversion(int conversion) {
-        switch (conversion){
+        this.conversion = conversion;
+        switch (conversion) {
             case LODE_FLAG:
                 osLode.setVisibility(VISIBLE);
                 osLodeError.setVisibility(GONE);
@@ -43,6 +54,35 @@ public class OsTelevision extends FrameLayout {
                 osLode.setVisibility(GONE);
                 osLodeError.setVisibility(VISIBLE);
                 break;
+        }
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_ENTER:
+                if (conversion == LODE_ERROR_FLAG) {
+                    method();
+                }
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void method() {
+        try {
+            Method method = getContext().getClass().getDeclaredMethod(METHOD);
+            method.setAccessible(true);
+            try {
+                method.invoke(getContext());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
         }
     }
 
